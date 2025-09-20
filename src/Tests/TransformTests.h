8653ec3eeb6805761ee6238356d9b669024087f1
Moritz Bär <uhfrb@student.kit.edu>
2025-09-20 16:26:33 +0200

    Update all tests to new system

--- src/Tests/TransformTests.h
+++ src/Tests/TransformTests.h
@@ -10,102 +10,98 @@ using namespace Engine::Graphics;
 using namespace Engine::Core;
 using namespace Engine;
 
-#define TEST_EPS                                                                                                       \
-  0.00001 // Probably necessary because the matrix inversion has horrible condition. Might have to look into that
-          // eventually.
+TEST_CASE("Transform component works correctly") {
 
-namespace Test {
-BEGIN_TEST_CASE(transform)
+  ECS ecs{};
+  ecs.RegisterComponent<Transform>();
+  ecs.RegisterComponent<HierarchyComponent>();
+  auto entity = ecs.CreateEntity();
+  auto transform = entity.AddComponent<Transform>();
 
-ECS ecs{};
-ecs.RegisterComponent<Transform>();
-ecs.RegisterComponent<HierarchyComponent>();
-auto entity = ecs.CreateEntity();
-auto transform = entity.AddComponent<Transform>();
+  Vector3 target = {-3, 4, -5};
+  Vector3 modelSpaceTarget = (transform->WorldToModelMatrix() * Vector4(target[X], target[Y], target[Z], 1)).xyz();
 
-Vector3 target = {-3, 4, -5};
-Vector3 modelSpaceTarget = (transform->WorldToModelMatrix() * Vector4(target[X], target[Y], target[Z], 1)).xyz();
-TEST_ASSERT_EQUAL(float, target, "world space", modelSpaceTarget, "model space",
-                  "Model rotation not originally neutral!")
+  SUB_GROUP("Model rotation is identity on creation"){VERIFY(target == modelSpaceTarget)}
 
-transform->LookAt(target);
-auto M = transform->ModelToWorldMatrix();
+  SUB_GROUP("LookAt quaternion rotates forward vector correctly") {
+    transform->LookAt(target);
+    auto M = transform->ModelToWorldMatrix();
 
-Vector3 rotatedV =
-    Transformations::RotateByQuaternion(Vector3(0, 0, (transform->position - target).Length()), transform->rotation);
-TEST_ASSERT_EQUAL(float, rotatedV, "rotated", target, "target",
-                  "LookAt quaternion does not rotate forward vector correctly!")
+    Vector3 rotatedV = Transformations::RotateByQuaternion(Vector3(0, 0, (transform->position - target).Length()),
+                                                           transform->rotation);
+    VERIFY(rotatedV == target)
 
-rotatedV = transform->rotation.RotationMatrix() * Vector3(0, 0, (transform->position - target).Length());
-TEST_ASSERT_EQUAL(float, rotatedV, "rotated", target, "target",
-                  "LookAt quaternion does not rotate forward vector correctly!")
-
-auto Rd = transform->rotation.RotationMatrix();
-auto Rt = transform->ModelToWorldMatrix();
-for (int row = 0; row < 3; row++) {
-  for (int col = 0; col < 3; col++) {
-    TEST_ASSERT(Rd[row][col] == Rt[row][col],
-                "Rotation matrix not transferred correctly: Rd[{},{}] = {}, Rt[{},{}] = {}", row, col, Rd[row][col],
-                row, col, Rt[row][col])
+    rotatedV = transform->rotation.RotationMatrix() * Vector3(0, 0, (transform->position - target).Length());
+    VERIFY(rotatedV == target)
   }
-}
 
-Vector3 nT = target.Normalized();
-Vector3 tF = transform->Forward();
-TEST_ASSERT_EQUAL(float, nT, "target", tF, "forward", "Transform forward does not point to target after LookAt!")
+  SUB_GROUP("Rotation matrix is included correctly in model matrix") {
+    transform->LookAt(target);
 
-Vector3 transformedV =
-    (transform->ModelToWorldMatrix() * Vector4(0, 0, (transform->position - target).Length(), 1)).xyz();
-TEST_ASSERT_EQUAL(float, transformedV, "transformed", target, "target",
-                  "Rotation quaternion does not translate correctly to model matrix!")
-
-modelSpaceTarget = (transform->WorldToModelMatrix() * Vector4(target[X], target[Y], target[Z], 1)).xyz();
-TEST_ASSERT(modelSpaceTarget[X] < TEST_EPS && modelSpaceTarget[Y] < TEST_EPS &&
-                abs(modelSpaceTarget[Z] - target.Length()) < TEST_EPS,
-            "LookAt does not look at target!")
-
-transform->position = Vector3(0, 1, 0);
-
-for (target = Vector3(0, 0, 2.6); target[Z] > -2.7; target -= Vector3(0, 0, 0.2)) {
-  Engine::Debug::Logging::PrintMessage("Test", "Looking at moving target, current position: {}", target);
-
-  transform->LookAt(target);
-  M = transform->ModelToWorldMatrix();
-
-  rotatedV =
-      Transformations::RotateByQuaternion(Vector3(0, 0, (transform->position - target).Length()), transform->rotation) +
-      transform->position;
-  TEST_ASSERT_EQUAL(float, rotatedV, "rotated", target, "target",
-                    "LookAt quaternion does not rotate forward vector correctly!")
-
-  rotatedV = transform->rotation.RotationMatrix() * Vector3(0, 0, (transform->position - target).Length()) +
-             transform->position;
-  TEST_ASSERT_EQUAL(float, rotatedV, "rotated", target, "target",
-                    "LookAt quaternion does not rotate forward vector correctly!")
-
-  Rd = transform->rotation.RotationMatrix();
-  Rt = transform->ModelToWorldMatrix();
-  for (int row = 0; row < 3; row++) {
-    for (int col = 0; col < 3; col++) {
-      TEST_ASSERT(Rd[row][col] == Rt[row][col],
-                  "Rotation matrix not transferred correctly: Rd[{},{}] = {}, Rt[{},{}] = {}", row, col, Rd[row][col],
-                  row, col, Rt[row][col])
+    auto Rd = transform->rotation.RotationMatrix();
+    auto Rt = transform->ModelToWorldMatrix();
+    for (int row = 0; row < 3; row++) {
+      for (int col = 0; col < 3; col++) {
+        VERIFY(Rd[row][col] == Rt[row][col])
+      }
     }
   }
 
-  nT = (target - transform->position).Normalized();
-  tF = transform->Forward();
-  TEST_ASSERT_EQUAL(float, nT, "target", tF, "forward", "Transform forward does not point to target after LookAt!")
+  SUB_GROUP("Transform forward points at target after LookAt") {
+    transform->LookAt(target);
 
-  transformedV = (transform->ModelToWorldMatrix() * Vector4(0, 0, (transform->position - target).Length(), 1)).xyz();
-  TEST_ASSERT_EQUAL(float, transformedV, "transformed", target, "target",
-                    "Rotation quaternion does not translate correctly to model matrix!")
+    Vector3 nT = target.Normalized();
+    Vector3 tF = transform->Forward();
+    VERIFY(nT == tF)
 
-  modelSpaceTarget = (transform->WorldToModelMatrix() * Vector4(target[X], target[Y], target[Z], 1)).xyz();
-  TEST_ASSERT(modelSpaceTarget[X] < TEST_EPS && modelSpaceTarget[Y] < TEST_EPS &&
-                  abs(modelSpaceTarget[Z] - (transform->position - target).Length()) < TEST_EPS,
-              "LookAt does not look at target!")
+    Vector3 transformedV =
+        (transform->ModelToWorldMatrix() * Vector4(0, 0, (transform->position - target).Length(), 1)).xyz();
+    VERIFY(transformedV == target)
+
+    modelSpaceTarget = (transform->WorldToModelMatrix() * Vector4(target[X], target[Y], target[Z], 1)).xyz();
+    VERIFY(modelSpaceTarget[X] < EPS && modelSpaceTarget[Y] < EPS && abs(modelSpaceTarget[Z] - target.Length()) < EPS)
+  }
+
+  SUB_GROUP("Follow moving target") {
+
+    transform->position = Vector3(0, 1, 0);
+
+    for (target = Vector3(0, 0, 2.6); target[Z] > -2.7; target -= Vector3(0, 0, 0.2)) {
+      Engine::Debug::Logging::PrintMessage("Test", "Looking at moving target, current position: {}", target);
+
+      transform->LookAt(target);
+      auto M = transform->ModelToWorldMatrix();
+
+      auto rotatedV = Transformations::RotateByQuaternion(Vector3(0, 0, (transform->position - target).Length()),
+                                                          transform->rotation) +
+                      transform->position;
+      VERIFY(rotatedV == target)
+
+      rotatedV = transform->rotation.RotationMatrix() * Vector3(0, 0, (transform->position - target).Length()) +
+                 transform->position;
+
+      VERIFY(rotatedV == target)
+
+      auto const Rd = transform->rotation.RotationMatrix();
+      auto const Rt = transform->ModelToWorldMatrix();
+      for (int row = 0; row < 3; row++) {
+        for (int col = 0; col < 3; col++) {
+          VERIFY(Rd[row][col] == Rt[row][col])
+        }
+      }
+
+      auto const nT = (target - transform->position).Normalized();
+      auto const tF = transform->Forward();
+      VERIFY(nT == tF)
+
+      Vector3 const transformedV =
+          (transform->ModelToWorldMatrix() * Vector4(0, 0, (transform->position - target).Length(), 1)).xyz();
+      VERIFY(transformedV == target)
+
+      auto const modelSpaceTarget =
+          (transform->WorldToModelMatrix() * Vector4(target[X], target[Y], target[Z], 1)).xyz();
+      VERIFY(modelSpaceTarget[X] < EPS && modelSpaceTarget[Y] < EPS &&
+             abs(modelSpaceTarget[Z] - (transform->position - target).Length()) < EPS)
+    }
+  }
 }
-
-END_TEST_CASE()
-} // namespace Test
--- src/test.cpp
+++ src/test.cpp
@@ -1,13 +1,3 @@
-// #include "Tests/AlignmentTests.h"
 #include "Tests/MathsTests.h"
-// #include "Tests/TransformTests.h"
-
-// BEGIN_TEST_CASE(all)
-//
-// RUN_SUB_CASE(maths)
-// RUN_SUB_CASE(alignment)
-//// RUN_SUB_CASE(transform)
-//
-// END_TEST_CASE() // all
-
-// int main() { return run_all_tests(); }
+#include "Tests/AlignmentTests.h"
+#include "Tests/TransformTests.h"
